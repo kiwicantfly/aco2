d519c9b0a42257ff4cbb58f8b9d361ce7695ffbf
Jafar Touzeau <jafar.touzeau@telecom-sudparis.eu>
2025-04-01 15:17:12 +0200

    Ajouts d'images et première pub

--- script.js
+++ script.js
@@ -10,14 +10,14 @@ const enigmePersonne0 = {
 }
 
 const enigmePersonne = [
-    "Non c'est pas moi le blond, c'est l'autre",
-    "À cause du rouge et du livre, j'ai confondu avec le BDA...",
-    "Tu peux m'appeler Bob",
-    "Je suis pas en cosplay, je m'habille juste comme ça",
-    "Oui j'ai encore changé de couleur de cheveux...",
-    "Boire n'est pas qu'une passion, c'est une profession.",
-    "J'ai toujours été friendly surtout avec mes potes… Tard dans la nuit colorée, la situation peut vite déraper",
-    "Parler ? C'est plus qu'un soft skill, c'est une philosophie de vivre."
+    ["Non c'est pas moi le blond, c'est l'autre", "fred.png"],
+    ["À cause du rouge et du livre, j'ai confondu avec le BDA...", "bob.jpg"],
+    ["Tu peux m'appeler Bob", "bob.jpg"],
+    ["Je suis pas en cosplay, je m'habille juste comme ça", "bob.jpg"],
+    ["Oui j'ai encore changé de couleur de cheveux...", "rainbow.png"],
+    ["Boire n'est pas qu'une passion, c'est une profession.", "boire.png"],
+    ["J'ai toujours été friendly surtout avec mes potes… Tard dans la nuit colorée, la situation peut vite déraper", "nuit.jpg"],
+    ["Parler ? C'est plus qu'un soft skill, c'est une philosophie de vivre.", "philo.png"]
 ]
 
 class Window extends HTMLElement {
@@ -86,7 +86,7 @@ class Popup extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <button style="display: flex; justify-content: center; align-items: center" class="close-button"><span style="font-family: monospace">×</span></button>
-        `;
+            `;
         const closeButton = this.querySelector(".close-button");
         closeButton.addEventListener("click", () => {
             this.close();
@@ -114,14 +114,14 @@ const suffleEnigmes = shuffle(enigmePersonne);
 for (let i=0; i<4; i++) {
     main.innerHTML += `
         <c-banner image="images/banners/banner${i+1}.png"></c-Banner>
-        <c-window id="window${2*i+1}" numero="${2*i+1}" enigme-personne-content="${suffleEnigmes[2*i]}"></c-window>
-        <c-window id="window${2*(i+1)}" numero="${2*(i+1)}" class="reverse" enigme-personne-content="${suffleEnigmes[2*i+1]}"></c-window>
+        <c-window id="window${2*i+1}" numero="${2*i+1}" enigme-personne-content="${suffleEnigmes[2*i][0]}" image="images/people/${suffleEnigmes[2*i][1]}"></c-window>
+        <c-window id="window${2*(i+1)}" numero="${2*(i+1)}" class="reverse" enigme-personne-content="${suffleEnigmes[2*i+1][0]}" image="images/people/${suffleEnigmes[2*i+1][1]}"></c-window>
     `
 }
 main.innerHTML += `<p>Merci Paolo pour ton idée de génie.</p>`;
 
 
-/* Apparition des popup toutes les 30s */
+/* Apparition des popup toutes les 20s */
 const couleurs = ['green', 'blue', 'red'];
 const shuffleCouleurs = shuffle(couleurs);
 const popup = document.getElementById("popup");
@@ -131,12 +131,11 @@ for (let i=0; i<3; i++) {
         popup.style.animationName = 'Disparition';
         setTimeout(
             ()=>{
-                popup.style.display = 'block';
+                popup.style.display = 'flex';
                 popup.style.backgroundColor = shuffleCouleurs[i];
                 popup.style.animationName = 'Apparition';
             },
             500
         )
-        console.log(shuffleCouleurs[i]);
     }, 20000*i+5000);
 };
